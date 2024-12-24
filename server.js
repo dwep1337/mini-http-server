85d0385076c2dc27a511c52ae820e7358969a2ab
Dwep <125664448+dwep1337@users.noreply.github.com>
2024-12-24 15:25:49 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+/* 
+    Mini HTTP server to redirect for https://guns.lol/dwep
+*/
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(301, { Location: "https://guns.lol/dwep" });
+  res.end();
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Done listening on port ${PORT}!`);
+});
